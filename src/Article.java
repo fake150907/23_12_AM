1c46b3c7a87d14879e73485b32b1c1df11300b73
fake150907 <yungayeon223@gmail.com>
2023-12-29 13:15:28 +0900

    게시글 작성 기능 추가 및 YGY_VER추가

---
+++ src/Article.java
@@ -0,0 +1,11 @@
+
+public class Article {
+	String title;
+	String body;
+	int lasteArticleId;
+	public Article(String title, String body, int lasteArticleId) {
+		this.title = title;
+		this.body = body;
+		this.lasteArticleId = lasteArticleId;
+	}
+}
