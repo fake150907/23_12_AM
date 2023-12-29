e6cc40746138d7148f0e89231bca3c3a4f76bbfe
fake150907 <yungayeon223@gmail.com>
2023-12-29 13:29:41 +0900

    게시물 목록 기능 구현

--- src/Article.java
+++ src/Article.java
@@ -1,11 +1,35 @@
+class Article {
+	private int id;
+	private String title;
+	private String body;
 
-public class Article {
-	String title;
-	String body;
-	int lasteArticleId;
-	public Article(String title, String body, int lasteArticleId) {
+	public Article(int id, String title, String body) {
+		this.id = id;
 		this.title = title;
 		this.body = body;
-		this.lasteArticleId = lasteArticleId;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -7,38 +7,49 @@ public class Main {
 		System.out.println("== 프로그램 시작 == ");
 
 		Scanner sc = new Scanner(System.in);
+
 		int lastArticleId = 0;
-		Article content;
-		List<Article> articlelist = new ArrayList<>();
+		List<Article> articles = new ArrayList<>();
+
 		while (true) {
 			System.out.print("명령어 > ");
-			String cmd = sc.nextLine();
+			String cmd = sc.nextLine().trim();
+
+			if (cmd.length() == 0) {
+				System.out.println("명령어를 입력하세요");
+				continue;
+			}
 
 			if (cmd.equals("exit")) {
 				break;
-			} else if (cmd.equals("article write")) {
+			}
+			if (cmd.equals("article write")) {
+				System.out.println("==게시글 작성==");
 				int id = lastArticleId + 1;
 				System.out.print("제목 : ");
 				String title = sc.nextLine();
 				System.out.print("내용 : ");
 				String body = sc.nextLine();
-				
-				System.out.printf("%d번 글이 생성 되었습니다.\n", id);
-				content = new Article(title, body, id);
-				lastArticleId++;
-				
-				articlelist.add(content);
-			} else if (cmd.equals("article list")) {
 
-				System.out.println("번호  |  제목  |  내용");
-				
-				for (int i = articlelist.size() - 1; i >= 0; i--) {
-					Article list = articlelist.get(i);
-					System.out.printf("%d  |  %s  |  %s\n", list.lasteArticleId, list.title, list.body);
+				Article article = new Article(id, title, body);
+				articles.add(article);
+//				System.out.println(title + " / " + body);
+
+				System.out.printf("%d번 글이 생성 되었습니다.\n", id);
+				lastArticleId++;
+			} else if (cmd.equals("article list")) {
+				System.out.println("==게시글 목록==");
+				if (articles.size() == 0) {
+					System.out.println("아무것도 없어");
+				} else {
+					System.out.println("  번호  /  제목  ");
+					for (int i = articles.size() - 1; i >= 0; i--) {
+						Article article = articles.get(i);
+						System.out.printf("  %4d  /   %s  \n", article.getId(), article.getTitle());
+					}
 				}
-				
 			} else {
-				System.out.println("명령을 똑바로 내려주세요 주인님.");
+				System.out.println("사용할 수 없는 명령어입니다");
 			}
 		}
 
